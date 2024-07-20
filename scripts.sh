cabe8703da5ce61332ec6ee83471cbc93e2044fa
Dennis Appiah <66980358+dennisappiah@users.noreply.github.com>
2024-07-20 14:48:02 +0000

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -24,3 +24,13 @@ sudo ln -s /opt/pycharm-*/bin/pycharm.sh /usr/local/bin/pycharm
 sudo apt-get install libxrender1
 sudo apt-get install libxext6 libxi6 libxrender1 libxtst6 libfreetype6
 
+
+# create rsa key pair
+openssl genrsa -out keypair.pem 2048
+
+# extract public key
+openssl rsa -in keypair.pem -pubout -out public.pem
+
+# create private key in PKCS#8 format
+openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in keypair.pem -out private.pem
+
